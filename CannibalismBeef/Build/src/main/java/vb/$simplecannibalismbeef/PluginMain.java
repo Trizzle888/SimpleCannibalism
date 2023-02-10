00d932dfa78e713bef20c69a1761b5c4dd1b9f0c
KevlarProps <69110458+Trizzle888@users.noreply.github.com>
2023-02-10 16:13:38 -0500

    Add files via upload

---
+++ CannibalismBeef/Build/src/main/java/vb/$simplecannibalismbeef/PluginMain.java
@@ -0,0 +1,102 @@
+package vb.$simplecannibalismbeef;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import org.bukkit.*;
+import org.bukkit.command.*;
+import org.bukkit.event.*;
+import org.bukkit.plugin.java.*;
+
+public class PluginMain extends JavaPlugin implements Listener {
+
+	private static PluginMain instance;
+
+	@Override
+	public void onEnable() {
+		instance = this;
+		getServer().getPluginManager().registerEvents(this, this);
+		try {
+			((org.bukkit.command.CommandSender) (Object) ((org.bukkit.command.ConsoleCommandSender) org.bukkit.Bukkit
+					.getConsoleSender())).sendMessage("Cannibalism enabled, feast on eachother.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] commandArgs) {
+		if (command.getName().equalsIgnoreCase("scb")) {
+			try {
+				org.bukkit.Bukkit.broadcastMessage("Cannibalism enabled, begin the feast.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+		return true;
+	}
+
+	public static void procedure(String procedure, List procedureArgs) throws Exception {
+	}
+
+	public static Object function(String function, List functionArgs) throws Exception {
+		return null;
+	}
+
+	public static List createList(Object obj) {
+		if (obj instanceof List) {
+			return (List) obj;
+		}
+		List list = new ArrayList<>();
+		if (obj.getClass().isArray()) {
+			int length = java.lang.reflect.Array.getLength(obj);
+			for (int i = 0; i < length; i++) {
+				list.add(java.lang.reflect.Array.get(obj, i));
+			}
+		} else if (obj instanceof Collection<?>) {
+			list.addAll((Collection<?>) obj);
+		} else if (obj instanceof Iterator) {
+			((Iterator<?>) obj).forEachRemaining(list::add);
+		} else {
+			list.add(obj);
+		}
+		return list;
+	}
+
+	public static void createResourceFile(String path) {
+		Path file = getInstance().getDataFolder().toPath().resolve(path);
+		if (Files.notExists(file)) {
+			try (InputStream inputStream = PluginMain.class.getResourceAsStream("/" + path)) {
+				Files.createDirectories(file.getParent());
+				Files.copy(inputStream, file);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static PluginMain getInstance() {
+		return instance;
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void event1(org.bukkit.event.entity.PlayerDeathEvent event) throws Exception {
+		((org.bukkit.World) ((org.bukkit.entity.Entity) event.getEntity()).getWorld()).dropItemNaturally(
+				((org.bukkit.Location) ((org.bukkit.entity.Entity) event.getEntity()).getLocation()),
+				PluginMain.getNamedItem(((org.bukkit.Material) org.bukkit.Material.BEEF), "Human Flesh"));
+	}
+
+	public static org.bukkit.inventory.ItemStack getNamedItem(Material material, String name) {
+		org.bukkit.inventory.ItemStack item = new org.bukkit.inventory.ItemStack(material);
+		org.bukkit.inventory.meta.ItemMeta itemMeta = item.getItemMeta();
+		if (itemMeta != null) {
+			itemMeta.setDisplayName(name);
+			item.setItemMeta(itemMeta);
+		}
+		return item;
+	}
+}
